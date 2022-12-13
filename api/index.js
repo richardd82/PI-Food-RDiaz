99f1cc2eba37380f73d805499fd66eb25cb3b6ba
Richardd82 <richardd82@gmail.com>
2022-12-12 18:37:39 -0600

    Deploy Backend 2

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 //Alter to save de data into the database
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
