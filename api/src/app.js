812df26784e66de5087a26189e295700f4aa11b5
Richardd82 <richardd82@gmail.com>
2022-12-12 22:24:28 -0600

    Deploy Back Env Variables2

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const {URL_FRONT} = process.env;
 
 require('./db.js');
 
@@ -15,7 +17,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://food.richardd82.dev'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', URL_FRONT); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
